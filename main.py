5db08d8c61317ea81b79225d9dafb3d7646529b7
jolea06 <joleatkinson@gmail.com>
2026-01-09 13:20:59 -0600

    EQ.m connected and functioning

--- main.py
+++ main.py
@@ -4,18 +4,20 @@ from PyQt6.QtWidgets import (QApplication, QWidget, QLabel,
                              QPushButton, QVBoxLayout, QLineEdit, 
                              QGridLayout, QHBoxLayout, QSlider)
 
-eng = matlab.engine.start_matlab() # Start MATLAB engine
-
 # APP UI
 class EQApp(QWidget):
     def __init__(self):
+        self.eng = matlab.engine.start_matlab() # Start MATLAB engine
+        self.eng.addpath(r"C:\Users\jolea\OneDrive\Documents\MATLAB", nargout=0) # Add path to MATLAB functions
         super().__init__()
         self.setWindowTitle("EQ")
-        self.resize(400, 800)
+        self.resize(1000, 1000)
         self.setMinimumSize(200, 200)
 
         self.mainLayout = QVBoxLayout() # Main Layout for the app
         self.sliderLayout = QHBoxLayout() # Layout for the sliders
+        self.labelLayout = QHBoxLayout() # Layout for the labels
+        self.sliderLabelLayout = QVBoxLayout() # Layout for slider labels
         self.buttonLayout = QHBoxLayout() # Layout for the buttons
 
         self.sliderList = [] # Create list to hold sliders
@@ -25,24 +27,50 @@ class EQApp(QWidget):
             slider.setTickInterval(1)
             slider.setMinimum(-20)
             slider.setMaximum(10)
-            slider.setFixedHeight(200)
+            slider.setFixedHeight(150)
+            slider.valueChanged.connect(self.sliderValues) # Connect slider to function
             self.sliderList.append(slider)
             self.sliderLayout.addWidget(slider)
+        toneDownSlider = QSlider() # Create Tone Down Slider
+        toneDownSlider.setSingleStep(1)
+        toneDownSlider.setTickInterval(1)
+        toneDownSlider.setMinimum(-10)
+        toneDownSlider.setMaximum(0)
+        toneDownSlider.setFixedHeight(100)
+        toneDownSlider.valueChanged.connect(self.sliderValues) # Connect slider to function
+        self.sliderList.append(toneDownSlider)
+        self.sliderLayout.addWidget(toneDownSlider)
 
         eqButton = QPushButton("Start EQ") # Create EQ Button 
         eqButton.setFixedSize(100, 50)
         self.buttonLayout.addWidget(eqButton)
 
-        self.mainLayout.addLayout(self.sliderLayout, 0)
+        sliderNames = ["Sub-Bass (20-60Hz)", "Bass (60-250Hz)", "Lower Mids (250-500Hz)",
+                       "Midrange (500Hz-2kHz)", "Upper Mids (2kHz-4kHz)", "Presence (4kHz-6kHz)", 
+                       "Brilliance/Air (6kHz-20kHz)", "Tone Down"]
+        
+        for i in range(8):
+            label = QLabel(f"{sliderNames[i]}")
+            self.labelLayout.addWidget(label)
+
+        self.mainLayout.addLayout(self.sliderLayout) # Add slider layout to main layout
+        self.mainLayout.addLayout(self.labelLayout, 7)
         self.mainLayout.addLayout(self.buttonLayout, 1)
 
         self.setLayout(self.mainLayout) # Use Main Layout for current window
 
         eqButton.clicked.connect(self.startEQ)
 
-
     def startEQ(self): # Function to start EQ process
+        self.eng.EQ(r"C:\Users\jolea\Downloads\cokkie.mp3", self.gainValues, nargout=0)
         print("Start EQ button was clicked")
+
+    def sliderValues(self): # Function to get slider values
+        currentVals = []
+        for i in range(8):
+            currentVals.append(self.sliderList[i].value())
+            print(f"Slider {i} value: {currentVals[i]}")
+        self.gainValues = matlab.double(currentVals) # Convert to MATLAB double array
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
