291babef593536ae3e8a13c31bf5cee1a2e6d33e
jolea06 <joleatkinson@gmail.com>
2026-01-08 18:28:31 -0600

    main created with sliders and basic button

---
+++ main.py
@@ -0,0 +1,51 @@
+import matlab.engine
+import sys
+from PyQt6.QtWidgets import (QApplication, QWidget, QLabel, 
+                             QPushButton, QVBoxLayout, QLineEdit, 
+                             QGridLayout, QHBoxLayout, QSlider)
+
+eng = matlab.engine.start_matlab() # Start MATLAB engine
+
+# APP UI
+class EQApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("EQ")
+        self.resize(400, 800)
+        self.setMinimumSize(200, 200)
+
+        self.mainLayout = QVBoxLayout() # Main Layout for the app
+        self.sliderLayout = QHBoxLayout() # Layout for the sliders
+        self.buttonLayout = QHBoxLayout() # Layout for the buttons
+
+        self.sliderList = []
+        for i in range(7):
+            slider = QSlider()
+            slider.setSingleStep(1)
+            slider.setTickInterval(1)
+            slider.setMinimum(-20)
+            slider.setMaximum(10)
+            slider.setFixedHeight(200)
+            self.sliderList.append(slider)
+            self.sliderLayout.addWidget(slider)
+
+        eqButton = QPushButton("Start EQ")
+        eqButton.setFixedSize(100, 50)
+        self.buttonLayout.addWidget(eqButton)
+
+        self.mainLayout.addLayout(self.sliderLayout, 0)
+        self.mainLayout.addLayout(self.buttonLayout, 1)
+
+        self.setLayout(self.mainLayout) # Use Main Layout for current window
+
+        eqButton.clicked.connect(self.startEQ)
+
+
+    def startEQ(self):
+        print("Start EQ button was clicked")
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = EQApp()
+    window.show()
+    sys.exit(app.exec())
